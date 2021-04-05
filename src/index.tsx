a432c52ed4d2a65274f4ab73a803b96d311079e3
devtayls <kelsey.taylor.pine@gmail.com>
2021-04-04 23:14:59 -0600

    Adds typescript static typing to the application

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 import './index.css';
 
-function calculateWinner(squares) {
-    const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
-    for (let i = 0; i< lines.length; i++) {
-        const [a, b, c] = lines[i];
-        if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
-        }
-    }
-    return null;
+interface SquareProps {
+    value: string | null,
+    onClick: () => void
 }
 
-function Square(props) {
+type Square = (string|null)[]
+
+function Square(props : SquareProps): JSX.Element {
     return (
         <button className={'square'} onClick={props.onClick}>
             {props.value}
@@ -30,8 +17,13 @@ function Square(props) {
     )
 }
 
-class Board extends React.Component {
-    renderSquare(i) {
+interface BoardProps {
+    squares: Square
+    onClick: (i: number) => void,
+}
+
+class Board extends React.Component <BoardProps, any> {
+    renderSquare(i : number) {
         return (
             <Square
                 value={this.props.squares[i]}
@@ -63,8 +55,14 @@ class Board extends React.Component {
     }
 }
 
-class Game extends React.Component {
-    constructor(props) {
+interface GameState {
+    xIsNext: boolean,
+    stepNumber: number,
+    history: {squares: Square}[]
+}
+
+class Game extends React.Component <any, GameState> {
+    constructor(props : any) {
         super(props);
         this.state = {
             history: [{
@@ -77,7 +75,7 @@ class Game extends React.Component {
         this.jumpTo = this.jumpTo.bind(this);
 
     }
-    handleClick(i) {
+    handleClick(i : number) {
         const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
@@ -96,12 +94,13 @@ class Game extends React.Component {
         });
     }
 
-    jumpTo(step) {
+    jumpTo(step: any) {
         this.setState({
             stepNumber: step,
             xIsNext: (step % 2) === 0,
         })
     }
+
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
@@ -140,7 +139,25 @@ class Game extends React.Component {
   }
 }
 
-// ========================================
+function calculateWinner(squares: any) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i< lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
 
 ReactDOM.render(
   <Game />,
