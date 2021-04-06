56b700d55fec50b0be426d4a744f3ee9cd7673a1
devtayls <kelsey.taylor.pine@gmail.com>
2021-04-05 18:55:43 -0600

    Adds a wrapper component for app.

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,17 @@ import * as ReactDOM from "react-dom";
 import Game from './components/game'
 import './index.css';
 
+class App extends React.Component{
+    render() {
+        return(
+            <div>
+                <Game />
+            </div>
+        )
+    }
+}
+
 ReactDOM.render(
-  <Game />,
+  <App />,
   document.getElementById('root')
 );
